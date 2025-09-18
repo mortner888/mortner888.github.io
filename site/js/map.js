c503c2f51dbfdd3ceb83bfa33c8a81bdb8656899
mortner888 <riccardoprete4@gmail.com>
2025-09-18 21:09:21 +0200

    a

--- site/js/map.js
+++ site/js/map.js
@@ -1,11 +1,11 @@
 const map = document.getElementById('map');
 
 let isDragging = false;
-let startX, startY;
-let currentX = 0, currentY = 0;
+let startX = 0;
+let startY = 0;
+let currentX = 0;
+let currentY = 0;
 let scale = 1;
-let minScale = 1;
-let maxScale = 2;
 
 map.addEventListener('mousedown', e => {
     isDragging = true;
@@ -23,10 +23,10 @@ document.addEventListener('mousemove', e => {
     if (!isDragging) return;
     currentX = e.clientX - startX;
     currentY = e.clientY - startY;
-    map.style.transform = `translate(${currentX}px, ${currentY}px) scale(${scale})`;
+    map.style.transform = `translate(calc(-50% + ${currentX}px), calc(-50% + ${currentY}px)) scale(${scale})`;
 });
 
 map.addEventListener('dblclick', () => {
     scale = scale === 1 ? 1.5 : 1;
-    map.style.transform = `translate(${currentX}px, ${currentY}px) scale(${scale})`;
+    map.style.transform = `translate(calc(-50% + ${currentX}px), calc(-50% + ${currentY}px)) scale(${scale})`;
 });
